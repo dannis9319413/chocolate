620c1114519889a068fbba2f2228be255af6a4ab
unknown <dannis9319413@gmail.com>
2024-01-16 10:04:37 +0800

    adjust migration

--- database/migrations/2024_01_10_082908_create_distributors_table.php
+++ database/migrations/2024_01_10_082908_create_distributors_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('distributors', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('bank_account');
             $table->timestamps();
         });
     }
